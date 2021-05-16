74efcc8487f651fb17abcdc994de7c58f0c495bb
AstroBoy73 <vijibalaji2003@gmail.com>
2021-05-16 22:51:19 +0530

    display prop for collapsible bar

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var navlinks = document.getElementById("navlinks")
+navlinks.style.right = "-200px";
+navlinks.style.display = "none";
 function closeMenu(){
   navlinks.style.transition = "1s";
   navlinks.style.right = "-200px";
+  navlinks.style.display = "none";
 }
 function showMenu(){
   navlinks.style.transition = "1s";
   navlinks.style.right = "0";
+  navlinks.style.display = "block";
 }
