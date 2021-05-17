8566048e3d90fc21ac7ca8d54b98f0fed9cad333
AstroBoy73 <vijibalaji2003@gmail.com>
2021-05-17 14:04:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var navlinks = document.getElementById("navlinks")
-navlinks.style.right = "-200px";
+// navlinks.style.right = "-200px";
 navlinks.style.opacity = "0";
 function closeMenu(){
   navlinks.style.transition = "1s";
-  navlinks.style.right = "-200px";
+  // navlinks.style.right = "-200px";
   navlinks.style.opacity = "0";
 }
 function showMenu(){
