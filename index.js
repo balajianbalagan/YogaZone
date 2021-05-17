22703101c18f6c9cf04d81ccca71df2ff364e30c
AstroBoy73 <vijibalaji2003@gmail.com>
2021-05-17 13:47:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var navlinks = document.getElementById("navlinks")
 navlinks.style.right = "-200px";
-navlinks.style.display = "none";
+navlinks.style.opacity = "0";
 function closeMenu(){
   navlinks.style.transition = "1s";
   navlinks.style.right = "-200px";
-  navlinks.style.display = "none";
+  navlinks.style.opacity = "0";
 }
 function showMenu(){
   navlinks.style.transition = "1s";
   navlinks.style.right = "0";
-  navlinks.style.display = "block";
+  navlinks.style.opacity = "1";
+}
+if(window.innerWidth<=700){
+  showMenu();
+  closeMenu();
+}
+if(window.innerWidth>800){
+  closeMenu();
+  showMenu();
 }
