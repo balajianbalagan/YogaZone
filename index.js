7064abf25e2fca460fa6f43309de0c8503bc22fd
AstroBoy73 <42893328+balajianbalagan@users.noreply.github.com>
2021-05-22 09:42:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ navlinks.style.right = "-200px";
 navlinks.style.opacity = "0";
 function closeMenu(){
   navlinks.style.transition = "1s";
-  
+  navlinks.style.right = "-200px";
   navlinks.style.opacity = "0";
 }
 function showMenu(){
   navlinks.style.transition = "1s";
   navlinks.style.right = "0";
   navlinks.style.opacity = "1";
-}
-if(window.innerWidth<=700){
-  showMenu();
-  closeMenu();
-}
-if(window.innerWidth>800){
-  closeMenu();
-  showMenu();
 }
