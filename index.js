663a2c54354da6a0a1dccf9a68a67e72a0cb876f
AstroBoy73 <42893328+balajianbalagan@users.noreply.github.com>
2021-05-22 09:55:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var navlinks = document.getElementById("navlinks")
 navlinks.style.right = "-200px";
-navlinks.style.opacity = "0";
+navlinks.style.opacity = "1";
 function closeMenu(){
   navlinks.style.transition = "1s";
   navlinks.style.right = "-200px";
